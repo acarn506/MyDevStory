a9cdda2d2ddf86737876ea9570547aeee595449d
acarn506 <acarnero@horizon.csueastbay.edu>
2020-06-02 13:58:33 -0700

    Added Pages

---
+++ reactapp/src/Pages/Async/Aysnc.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Async = () => {
+  return (
+    <div>
+      <h1>Async</h1>
+    </div>
+  );
+};
+
+export default Async;
---
+++ reactapp/src/Pages/Node/Node.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Node = () => {
+  return (
+    <div>
+      <h1>Node</h1>
+    </div>
+  );
+};
+
+export default Node;
