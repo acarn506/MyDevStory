ebef7c37bdbe92a2ecb77d044cc852dc5aef0854
acarn506 <acarnero@horizon.csueastbay.edu>
2020-06-03 16:45:34 -0700

    Layout Updated

--- reactapp/src/Pages/Home/Home.jsx
+++ reactapp/src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Home.css";
 
 const Home = props => {
   return (
-    <div>
+    <div className="homeContainer">
       <h1>Home</h1>
       <div>
         <ul>
