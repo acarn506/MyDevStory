6fab957685e8942dea346d56f1e458a0967ca47e
acarn506 <acarnero@horizon.csueastbay.edu>
2020-06-27 12:47:09 -0700

    Updated Dropdown

--- reactapp/src/Pages/ReactPage/Dropdown/Dropdown.jsx
+++ reactapp/src/Pages/ReactPage/Dropdown/Dropdown.jsx
@@ -1,14 +1,50 @@
 import React, { useState } from "react";
 import "./Dropdown.css";
+import ItemList from "../ItemList/ItemList";
 
-const Dropdown = props => {
+const Dropdown = () => {
   const [open, setOpen] = useState(false);
+  const DropdownItem = props => {
+    return (
+      <div className="dropContainer">
+        <h4 className="title">{props.title}</h4>
+        <span className="text">{props.text}</span>
+      </div>
+    );
+  };
   return (
     <div className="dropdown">
-      <a href="#" className="legend" onClick={() => setOpen(!open)}>
+      <button className="legend" onClick={() => setOpen(!open)}>
         Legend
-      </a>
-      <div className="menu-item">{open && props.children}</div>
+        <div className="menu-item">
+          {open && (
+            <React.Fragment>
+              <DropdownItem
+                title={"familiar"}
+                text={
+                  "Can make things work by collaging code snippets together"
+                }
+              />
+              <DropdownItem
+                title={"Proficient"}
+                text={
+                  "Occasional reference to Stack Overflow and external references"
+                }
+              />
+              <DropdownItem
+                title={"Excellent"}
+                text={
+                  "Can code without Stack Overflow and minimal references to documentation"
+                }
+              />
+              <DropdownItem
+                title={"Expert"}
+                text={"Understands internals of the language"}
+              />
+            </React.Fragment>
+          )}
+        </div>
+      </button>
     </div>
   );
 };
