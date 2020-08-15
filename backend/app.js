03e6933536da57ab9145ceb09c661a4cc45a4ddb
acarn506 <acarnero@horizon.csueastbay.edu>
2020-08-14 17:34:37 -0700

    cod route

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(6000);
+//dev logginh
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//profile routes
+app.use("/api/v1/profile", require("./routes/profile.js"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode and listening on port ${port}`
+  );
+});
---
+++ backend/routes/profile.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
+
+router.get("/:platform/:gamertag", async (req, res) => {
+  try {
+    const headers = {
+      "TRN-Api-Key": process.env.TRACKER_API_KEY
+    };
+    const { platform, gamertag } = req.params;
+
+    const response = await fetch(
+      `${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}?TRN-Api-Key=d217d2cc-8441-4639-83db-ee053fc8f697`,
+      { headers }
+    );
+    const data = await response.json();
+
+    if (data.errors && data.errors.length > 0) {
+      return res.status(404).json({
+        message: "Profile Not Found"
+      });
+    }
+
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "server error"
+    });
+  }
+});
+
+module.exports = router;
